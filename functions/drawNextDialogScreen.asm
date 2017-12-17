5d17250f8ab953778aa30c86f179865350b48bb7
Jonathan Moody <jonmoody12@gmail.com>
2017-12-17 16:18:28 -0500

    Fixed stutter on last dialog screen

--- functions/drawNextDialogScreen.asm
+++ functions/drawNextDialogScreen.asm
@@ -76,128 +76,15 @@ LoadIntro5:
   LDA #HIGH(backgroundDialogIntro1)
   STA pointerBackgroundHighByte
   JSR LoadBottomDialog
-  JMP EndLoadingDialogBackground
-
-; LoadIntro3:
-;   LDA currentDialogScreen
-;   CMP #$03
-;   BNE LoadIntro4
-;
-;   JSR WipeBottomDialog
-;   LDA #LOW(backgroundDialogIntro3)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro3)
-;   STA pointerBackgroundHighByte
-;   JSR LoadTopDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro4:
-;   LDA currentDialogScreen
-;   CMP #$04
-;   BNE LoadIntro5
-;
-;   JSR WipeTopDialog
-;   LDA #LOW(backgroundDialogIntro4)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro4)
-;   STA pointerBackgroundHighByte
-;   JSR LoadBottomDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro5:
-;   LDA currentDialogScreen
-;   CMP #$05
-;   BNE LoadIntro6
-;
-;   JSR WipeBottomDialog
-;   LDA #LOW(backgroundDialogIntro5)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro5)
-;   STA pointerBackgroundHighByte
-;   JSR LoadBottomDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro6:
-;   LDA currentDialogScreen
-;   CMP #$06
-;   BNE LoadIntro7
-;
-;   JSR WipeBottomDialog
-;   LDA #LOW(backgroundDialogIntro6)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro6)
-;   STA pointerBackgroundHighByte
-;   JSR LoadBottomDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro7:
-;   LDA currentDialogScreen
-;   CMP #$07
-;   BNE LoadIntro8
-;
-;   JSR WipeBottomDialog
-;   LDA #LOW(backgroundDialogIntro7)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro7)
-;   STA pointerBackgroundHighByte
-;   JSR LoadTopDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro8:
-;   LDA currentDialogScreen
-;   CMP #$08
-;   BNE LoadIntro9
-;
-;   JSR WipeTopDialog
-;   LDA #LOW(backgroundDialogIntro8)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro8)
-;   STA pointerBackgroundHighByte
-;   JSR LoadBottomDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro9:
-;   LDA currentDialogScreen
-;   CMP #$09
-;   BNE LoadIntro10
-;
-;   JSR WipeBottomDialog
-;   LDA #LOW(backgroundDialogIntro9)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro9)
-;   STA pointerBackgroundHighByte
-;   JSR LoadBottomDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro10:
-;   LDA currentDialogScreen
-;   CMP #$0A
-;   BNE LoadIntro11
-;
-;   JSR WipeBottomDialog
-;   LDA #LOW(backgroundDialogIntro10)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro10)
-;   STA pointerBackgroundHighByte
-;   JSR LoadTopDialog
-;   JMP EndLoadingDialogBackground
-;
-; LoadIntro11:
-;   LDA currentDialogScreen
-;   CMP #$0B
-;   BNE EndLoadingDialogBackground
-;
-;   JSR WipeTopDialog
-;   LDA #LOW(backgroundDialogIntro11)
-;   STA pointerBackgroundLowByte
-;   LDA #HIGH(backgroundDialogIntro11)
-;   STA pointerBackgroundHighByte
-;   JSR LoadBottomDialog
 
   LDA #$01
   STA endOfDialog
   LDA #$00
   STA forgeScene
+
+
+  
+
 EndLoadingDialogBackground:
   JSR EnableGraphics
   RTS
