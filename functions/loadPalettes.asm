31711c59c0aa3a788b8f36b8fcc5232ac7914973
Jonathan Moody <jonmoody12@gmail.com>
2018-01-03 00:41:18 -0500

    Adding a car background

--- functions/loadPalettes.asm
+++ functions/loadPalettes.asm
@@ -45,3 +45,19 @@ LoadTitlePalettes:
   CPX #$20
   BNE .Loop
   RTS
+
+LoadTeslaPalette:
+  LDA $2002
+  LDA #$3F
+  STA $2006
+  LDA #$00
+  STA $2006
+
+  LDX #$00
+.Loop:
+  LDA teslaPalette, x
+  STA $2007
+  INX
+  CPX #$20
+  BNE .Loop
+  RTS
