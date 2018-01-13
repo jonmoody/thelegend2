87b72fe0a35fdfd7145d0e6ec40ba9fa6bde96f8
Jonathan Moody <jonmoody12@gmail.com>
2018-01-13 12:42:01 -0500

    Forgot to add boss movement file

---
+++ functions/bossMovement.asm
@@ -0,0 +1,17 @@
+ExecuteBossMovement:
+
+  DEC travelerSprite1X
+  DEC travelerSprite2X
+  DEC travelerSprite3X
+  DEC travelerSprite4X
+  DEC travelerSprite5X
+  DEC travelerSprite6X
+  DEC travelerSprite7X
+  DEC travelerSprite8X
+  DEC travelerSprite9X
+  DEC travelerSprite10X
+  DEC travelerSprite11X
+  DEC travelerSprite12X
+
+EndExecuteBossMovement:
+  RTS
